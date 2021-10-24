848f166a9a2ba48cbdb7b8a22277ee5474769784
Divya-1200 <divya07msd@gmail.com>
2021-10-24 08:19:04 +0530

    Updates for 2021-10-24

--- newsapi/api-news.py
+++ newsapi/api-news.py
@@ -17,21 +17,7 @@ if response.status_code == 200:
    data = response.json()
 
    print(data)
-  
-#    main = data['main']
-  
-#    temperature = main['temp']
-  
-#    humidity = main['humidity']
-   
-#    pressure = main['pressure']
- 
-#    report = data['weather']
-#    print(f"{CITY:-^30}")
-#    print(f"Temperature: {temperature}")
-#    print(f"Humidity: {humidity}")
-#    print(f"Pressure: {pressure}")
-#    print(f"Weather Report: {report[0]['description']}")
+
 else:
-   # showing the error message
+
    print("Error in the HTTP request")
