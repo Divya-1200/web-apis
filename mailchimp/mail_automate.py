6e434f5e37795a6ba70595c1b0abe19760659f2d
Divya-1200 <divya07msd@gmail.com>
2021-10-24 08:27:47 +0530

    Updates for 2021-10-24

--- mailchimp/mail_automate.py
+++ mailchimp/mail_automate.py
@@ -1,9 +1,14 @@
 import mailchimp_transactional as MailchimpTransactional
 from mailchimp_transactional.api_client import ApiClientError
+from decouple import config
+
+API_KEY = config('API_KEY')
+
+print(API_KEY)
 
 def run():
   try:
-    mailchimp = MailchimpTransactional.Client('YOUR_API_KEY')
+    mailchimp = MailchimpTransactional.Client('API_KEY')
     response = mailchimp.users.ping()
     print('API called successfully: {}'.format(response))
   except ApiClientError as error:
